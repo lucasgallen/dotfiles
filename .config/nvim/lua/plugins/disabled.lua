195d0082b271cb847e7ad6845865441b101f76b8
Lucas Allen <lucas.g.allen@gmail.com>
2024-06-25 16:27:39 +0100

    Add disabled lua for lazyvim disabled plugins & settings

---
+++ .config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,11 @@
+return {
+  { "rcarriga/nvim-notify", enabled = false },
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        markdown = {},
+      },
+    },
+  },
+}
