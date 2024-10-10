1c1a272a0779b396aa744a92f40ff3080902f8e5
Lucas Allen <lucas.g.allen@gmail.com>
2024-10-10 06:21:15 +0000

    Add script to rename photos

--- scripts/rename-photos.sh
+++ scripts/rename-photos.sh
@@ -5,14 +5,14 @@ photo_dir=$1
 photo_prefix=$2
 
 (
-	cd $photo_dir
-	photos_count=$(ls | wc -l)
-	i=1
-	for photo in *; do
-		filetype=${photo##*.}
-		new_photo_name="$photo_prefix-$i.$filetype"
-		mv $photo $new_photo_name
-		echo $i
-		i=$((i + 1))
-	done
+  cd "$photo_dir"
+  photos_count=$(ls | wc -l)
+  i=1
+  for photo in *; do
+    filetype=${photo##*.}
+    new_photo_name="$photo_prefix-$i.$filetype"
+    mv $photo $new_photo_name
+    echo $i
+    i=$((i + 1))
+  done
 )
